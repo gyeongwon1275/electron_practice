bf63665e56f163c1de911b54b023dd1657ad2834
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-08-05 11:47:37 +0900

    ci test

--- src/dataProviders/__tests__/sample.test.ts
+++ src/dataProviders/__tests__/sample.test.ts
@@ -2,6 +2,6 @@ import { truthy } from "../sample";
 
 describe("smaple test", () => {
   it("sample boolean test!", () => {
-    expect(truthy()).toBeTruthy();
+    expect(truthy()).toBeFalsy();
   });
 });
