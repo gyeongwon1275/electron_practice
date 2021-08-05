cd4416bd9b82a6f7ebd8312b77baf62c58885e62
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-08-05 13:29:48 +0900

    cache test

--- src/dataProviders/__tests__/sample.test.ts
+++ src/dataProviders/__tests__/sample.test.ts
@@ -3,5 +3,6 @@ import { truthy } from "../sample";
 describe("smaple test", () => {
   it("sample boolean test!", () => {
     expect(truthy()).toBeTruthy();
+    expect(false).toBeFalsy();
   });
 });
